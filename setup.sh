5c4ca276b166f00be8ff3bcb144e2b39b3c46ccc
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-03-10 17:44:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e  # Exit on error
 
-echo "Building the .deb package..."
-dpkg --build discord-devros
-
-echo "Installing the package..."
-sudo dpkg -i discord-devros.deb
-
-echo "Setup complete! Starting the bot..."
-/usr/bin/start_bot
